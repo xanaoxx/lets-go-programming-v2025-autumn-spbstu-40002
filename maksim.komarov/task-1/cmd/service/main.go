d01cda498fb3310fd04275c97a01ac64ffbdf4da
MeguruMacabre <145839835+MeguruMacabre@users.noreply.github.com>
2025-10-03 14:01:36 +0300

    [TASK-1] console calculator (#23)

---
+++ maksim.komarov/task-1/cmd/service/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstOperand int
+	var secondOperand int
+	var operator string
+	var answer int
+
+	_, err := fmt.Scan(&firstOperand)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+
+	_, err = fmt.Scan(&secondOperand)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+
+	_, err = fmt.Scan(&operator)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operator {
+	case "+":
+		answer = firstOperand + secondOperand
+	case "-":
+		answer = firstOperand - secondOperand
+	case "*":
+		answer = firstOperand * secondOperand
+	case "/":
+		if secondOperand == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		answer = firstOperand / secondOperand
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	fmt.Println(answer)
+}
---
+++ maksim.komarov/task-1/go.mod
@@ -0,0 +1,3 @@
+module gitgub.com/megurumacabre/task-1
+
+go 1.22.7

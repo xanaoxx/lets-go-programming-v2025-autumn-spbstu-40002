f81d6393ad392254e3eadbee5a16a8c1f28c01b6
xanaoxx <znellaz25@gmail.com>
2025-10-18 02:44:16 +0300

    [TASK-2-1]

--- kseniya.varfolomeeva/task-2-1/cmd/service/main.go
+++ kseniya.varfolomeeva/task-2-1/cmd/service/main.go
@@ -3,39 +3,37 @@ package main
 import "fmt"
 
 func main() {
-    var departmentCount int
-    fmt.Scan(&departmentCount)
+	var departmentCount int
+	_, _ = fmt.Scan(&departmentCount)
 
-    for i := 0; i < departmentCount; i++ {
-        var employeesCount int
-        fmt.Scan(&employeesCount)
+	for i := 0; i < departmentCount; i++ {
+		var employeesCount int
+		_, _ = fmt.Scan(&employeesCount)
 
-        minTemp := 15
-        maxTemp := 30
+		minTemp := 15
+		maxTemp := 30
 
-        for j := 0; j < employeesCount; j++ {
-            var operator string
-            var temp int
-            fmt.Scan(&operator, &temp)
+		for j := 0; j < employeesCount; j++ {
+			var operator string
+			var temp int
+			_, _ = fmt.Scan(&operator, &temp)
 
-            if operator == ">=" {
-                if temp > minTemp {
-                    minTemp = temp
-                }
-            } else if operator == "<=" {
-                if temp < maxTemp {
-                    maxTemp = temp
-                }
-            }
+			switch operator {
+			case ">=":
+				if temp > minTemp {
+					minTemp = temp
+				}
+			case "<=":
+				if temp < maxTemp {
+					maxTemp = temp
+				}
+			}
 
-            if minTemp <= maxTemp {
-                fmt.Println(minTemp)
-            } else {
-                fmt.Println(-1)
-            }
-        }
-        if i < departmentCount-1 {
-            fmt.Println()
-        }
-    }
+			if minTemp <= maxTemp {
+				fmt.Println(minTemp)
+			} else {
+				fmt.Println(-1)
+			}
+		}
+	}
 }

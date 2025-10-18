c83ec44d503d8d247386c95a26f0bb103adb8d2a
xanaoxx <znellaz25@gmail.com>
2025-10-19 01:31:59 +0300

    [TASK-2-2]

--- kseniya.varfolomeeva/task-2-2/cmd/service/main.go
+++ kseniya.varfolomeeva/task-2-2/cmd/service/main.go
@@ -3,40 +3,76 @@ package main
 import (
 	"container/heap"
 	"fmt"
+	"os"
 )
 
 type MaxHeap []int
 
-func (h *MaxHeap) Len() int           { return len(*h) }
+func (h *MaxHeap) Len() int { return len(*h) }
+
 func (h *MaxHeap) Less(i, j int) bool { return (*h)[i] > (*h)[j] }
-func (h *MaxHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
-func (h *MaxHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+
+func (h *MaxHeap) Swap(i, j int) { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
+
+func (h *MaxHeap) Push(x interface{}) {
+	value, ok := x.(int)
+	if !ok {
+		return
+	}
+
+	*h = append(*h, value)
+}
+
 func (h *MaxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
 func main() {
-	var n int
-	fmt.Scan(&n)
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&arr[i])
+	var count int
+	_, err := fmt.Scan(&count)
+	if err != nil {
+		os.Exit(1)
 	}
-	var k int
-	fmt.Scan(&k)
 
-	h := &MaxHeap{}
-	heap.Init(h)
-	for _, num := range arr {
-		heap.Push(h, num)
+	numbers := make([]int, count)
+
+	for index := 0; index < count; index++ {
+		_, err = fmt.Scan(&numbers[index])
+		if err != nil {
+			os.Exit(1)
+		}
 	}
-	var res int
-	for i := 0; i < k; i++ {
-		res = heap.Pop(h).(int)
+
+	var order int
+	_, err = fmt.Scan(&order)
+	if err != nil {
+		os.Exit(1)
 	}
-	fmt.Println(res)
+
+	heapInstance := &MaxHeap{}
+	heap.Init(heapInstance)
+
+	for _, number := range numbers {
+		heap.Push(heapInstance, number)
+	}
+
+	var result int
+
+	for iteration := 0; iteration < order; iteration++ {
+		value := heap.Pop(heapInstance)
+		intValue, ok := value.(int)
+
+		if !ok {
+			os.Exit(1)
+		}
+
+		result = intValue
+	}
+
+	fmt.Println(result)
 }

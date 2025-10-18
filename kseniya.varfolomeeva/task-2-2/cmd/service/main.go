05803f0f4f68a07bacf8f8cd5e43c705d39ea7c1
xanaoxx <znellaz25@gmail.com>
2025-10-19 00:15:54 +0300

    [TASK-2-2]

--- kseniya.varfolomeeva/task-2-2/cmd/service/main.go
+++ kseniya.varfolomeeva/task-2-2/cmd/service/main.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 )
 
-type MinHeap []int
+type MaxHeap []int
 
-func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
-func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h MaxHeap) Len() int           { return len(h) }
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *MinHeap) Push(x interface{}) {
+func (h *MaxHeap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
 
-func (h *MinHeap) Pop() interface{} {
+func (h *MaxHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]
@@ -40,15 +40,14 @@ func main() {
 }
 
 func findKthSmallest(arr []int, k int) int {
-	h := &MinHeap{}
+	h := &MaxHeap{}
 	heap.Init(h)
 
 	for _, num := range arr {
 		heap.Push(h, num)
-	}
-
-	for i := 0; i < k-1; i++ {
-		heap.Pop(h)
+		if h.Len() > k {
+			heap.Pop(h)
+		}
 	}
 
 	return (*h)[0]

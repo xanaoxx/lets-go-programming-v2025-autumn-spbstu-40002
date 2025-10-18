db380843dd72064a45a4717ee9fbd236b9cecf50
xanaoxx <znellaz25@gmail.com>
2025-10-19 00:29:35 +0300

    [TASK-2-2]

--- kseniya.varfolomeeva/task-2-2/cmd/service/main.go
+++ kseniya.varfolomeeva/task-2-2/cmd/service/main.go
@@ -8,14 +8,14 @@ import (
 type MinHeap []int
 
 func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
 func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
 
-func (h *MaxHeap) Push(x interface{}) {
+func (h *MinHeap) Push(x interface{}) {
 	*h = append(*h, x.(int))
 }
 
-func (h *MaxHeap) Pop() interface{} {
+func (h *MinHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	x := old[n-1]

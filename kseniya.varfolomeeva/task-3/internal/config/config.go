0b8d49ab33cc70473ac67ee939fba31a24633f0f
xanaoxx <znellaz25@gmail.com>
2025-10-31 23:03:17 +0300

    [TASK-3]

--- kseniya.varfolomeeva/task-3/internal/config/config.go
+++ kseniya.varfolomeeva/task-3/internal/config/config.go
@@ -12,23 +12,19 @@ type AppConfig struct {
 	OutputFile string `yaml:"output-file"`
 }
 
-func LoadConfig(configPath string) (*AppConfig, error) { // ← ИЗМЕНИ НАЗВАНИЕ здесь!
+func LoadConfig(configPath string) (*AppConfig, error) {
 	if configPath == "" {
 		return nil, fmt.Errorf("config path is required")
 	}
 
-	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		return nil, fmt.Errorf("no such file or directory")
-	}
-
 	data, err := os.ReadFile(configPath)
 	if err != nil {
-		return nil, fmt.Errorf("cannot read config file: %w", err)
+		return nil, fmt.Errorf("no such file or directory")
 	}
 
 	var config AppConfig
 	if err := yaml.Unmarshal(data, &config); err != nil {
-		return nil, fmt.Errorf("did not find expected key: %w", err)
+		return nil, fmt.Errorf("did not find expected key")
 	}
 
 	return &config, nil

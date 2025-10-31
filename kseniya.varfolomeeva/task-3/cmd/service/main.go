3a108139312f51424a33b10158c1229fb7f26f4d
xanaoxx <znellaz25@gmail.com>
2025-10-31 23:07:42 +0300

    [TASK-3]

--- kseniya.varfolomeeva/task-3/cmd/service/main.go
+++ kseniya.varfolomeeva/task-3/cmd/service/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/xanaoxx/task-3/internal/config"
 	"github.com/xanaoxx/task-3/internal/json"
@@ -12,20 +13,27 @@ func main() {
 	configPath := flag.String("config", "", "path to config file")
 	flag.Parse()
 
+	fmt.Printf("DEBUG: Config path: %s\n", *configPath)
+
 	appConfig, err := config.LoadConfig(*configPath)
 	if err != nil {
-		panic("no such file or directory")
+		fmt.Printf("DEBUG: Config error: %v\n", err)
+		panic(err)
 	}
+
+	fmt.Printf("DEBUG: Input file: %s\n", appConfig.InputFile)
 
 	currencyRecords, err := xml.LoadCurrencies(appConfig.InputFile)
 	if err != nil {
-		panic("no such file or directory")
+		fmt.Printf("DEBUG: XML error: %v\n", err)
+		panic(err)
 	}
 
 	currencyRecords.SortByValue()
 
 	err = json.SaveCurrencies(currencyRecords, appConfig.OutputFile)
 	if err != nil {
+		fmt.Printf("DEBUG: JSON error: %v\n", err)
 		panic(err)
 	}
 }
